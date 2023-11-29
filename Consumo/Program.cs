c21a785267971f53038c7c17f6134e5fcb0675ae
Tony <antony.branquinho@gmail.com>
2023-11-29 12:50:54 -0300

    de primeira

---
+++ Consumo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumo {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int distancia;
+            double consumo, media;
+
+            Console.Write("Distancia percorrida: ");
+            distancia = int.Parse(Console.ReadLine(), CI);
+            Console.Write("Combustivel gasto: ");
+            consumo = double.Parse(Console.ReadLine(), CI);
+
+            media = distancia / consumo;
+
+            Console.WriteLine("Consumo medio: "+media.ToString("F3", CI));
+            
+        }
+    }
+}
